d2c6a478c8d000e70c4249fbb1e102497b044ee8
AMIR ALI <142417999+makesomediffert@users.noreply.github.com>
2024-04-24 16:18:57 +0530

    all types

---
+++ scrip.js
@@ -0,0 +1,9 @@
+// const rotateElement = document.querySelector('.rotate-element');
+
+// rotateElement.addEventListener('mouseover', () => {
+//   rotateElement.style.transform = 'rotate3d(0, 0, 0, 220deg)';
+// });
+
+// rotateElement.addEventListener('mouseout', () => {
+//   rotateElement.style.transform = 'rotate3d(0, 0, 0, 0deg)';
+// });
